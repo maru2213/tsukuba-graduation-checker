7ccb70b3765a8a3ae85434cf3e7cfa46bae0c1c6
maru2213 <25478531+maru2213@users.noreply.github.com>
2021-06-07 09:21:20 +0900

    jsonの参照先を自分のリポジトリに

--- src/main/kotlin/MigrationChecker.kt
+++ src/main/kotlin/MigrationChecker.kt
@@ -11,7 +11,7 @@ object MigrationChecker {
 
     // rule_definitions.jsonを読み込む
     fun loadRuleDefinitions() {
-        window.fetch(Request("https://raw.githubusercontent.com/itsu-dev/scs-migration-checker/master/src/main/resources/rule_definitions.json"))
+        window.fetch(Request("https://raw.githubusercontent.com/maru2213/scs-migration-checker/master/src/main/resources/rule_definitions.json"))
             .then(onFulfilled = {
                 it.text().then { json ->
                     onLoadFinished(json)
